fcdfb65650de4d77d7d184ccb79c43f61264e09d
Robert McDermott <robert.c.mcdermott@gmail.com>
2016-02-03 10:31:13 -0800

    updated index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Oncoscape!!!\n');
+  res.send('<h2>Hello Oncoscape!!!</h2>\n');
 });
 
 app.listen(PORT);
